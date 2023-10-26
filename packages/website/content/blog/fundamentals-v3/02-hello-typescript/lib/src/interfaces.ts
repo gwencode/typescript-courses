9d4ee11e3a9f62dc99c637a63fda28fa4922ca0e
Gwendal Le Bris <gwen.coding@gmail.com>
2023-10-26 17:53:59 +0200

    Interfaces Inheritence

--- packages/website/content/blog/fundamentals-v3/02-hello-typescript/lib/src/interfaces.ts
+++ packages/website/content/blog/fundamentals-v3/02-hello-typescript/lib/src/interfaces.ts
@@ -16,5 +16,70 @@ const user = {
   name: "Mike North",
   email: "mike@gmail.com",
 }
+// printUserInfo(user)
 
-printUserInfo(user)
+// Interface Inheritance
+
+interface Animal {
+  isAlive(): boolean
+}
+interface Mammal extends Animal {
+  getFurOrHairColor(): string
+}
+interface Dog extends Mammal {
+  getBreed(): string
+}
+function careForDog(dog: Dog) {
+  dog.getBreed
+}
+
+// Implements
+
+// interface AnimalLike {
+//   eat(food: string): void
+// }
+
+// class Dog implements AnimalLike {
+//   bark() {
+//     return "woof"
+//   }
+//   eat(food: string): string {
+//     return "eating " + food
+//   }
+// }
+
+// const aDog = new Dog()
+// console.log(aDog.bark())
+// console.log(aDog.eat("bacon"))
+
+
+class LivingOrganism {
+  isAlive() {
+    return true
+  }
+}
+interface AnimalLike {
+  eat(food: string): void
+}
+interface CanBark {
+  bark(): string
+}
+
+// Multiple Implements // Extends and Implements at the same time
+
+class Dog
+  extends LivingOrganism
+  implements AnimalLike, CanBark
+{
+  bark() {
+    return "woof"
+  }
+  eat(food: string): string {
+    return "eating " + food
+  }
+}
+
+const aDog = new Dog()
+console.log(aDog.bark())
+console.log(aDog.eat("bacon"))
+console.log(aDog.isAlive())
