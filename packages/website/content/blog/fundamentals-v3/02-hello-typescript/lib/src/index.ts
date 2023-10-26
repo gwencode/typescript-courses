8bcb0137bf86648886a2bc215dee25176fe3b196
Gwendal Le Bris <gwen.coding@gmail.com>
2023-10-26 14:36:09 +0200

    Lesson Index Signatures

--- packages/website/content/blog/fundamentals-v3/02-hello-typescript/lib/src/index.ts
+++ packages/website/content/blog/fundamentals-v3/02-hello-typescript/lib/src/index.ts
@@ -102,3 +102,21 @@ printCar({
   year: 2020,
   // color: 'blue' // Excess Property Check => Object literal may only specify known properties, and 'color' does not exist in type
 })
+
+// Index Signatures
+
+// const phones = {
+//   home: { country: "+1", area: "211", number: "652-4515" },
+//   work: { country: "+1", area: "670", number: "752-5856" },
+//   fax: { country: "+1", area: "322", number: "525-4357" },
+// }
+
+const phones: {
+  [k: string]: {
+    country: string
+    area: string
+    number: string
+  }
+} = {}
+
+phones.fax
