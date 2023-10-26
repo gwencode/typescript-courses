9f843d4a018968941889b714d2aed68f8ff7f66a
Gwendal Le Bris <gwen.coding@gmail.com>
2023-10-26 12:37:32 +0200

    Lession Variables & Values

--- packages/website/content/blog/fundamentals-v3/02-hello-typescript/lib/src/index.ts
+++ packages/website/content/blog/fundamentals-v3/02-hello-typescript/lib/src/index.ts
@@ -20,3 +20,29 @@ export async function addNumbers(a: number, b: number) {
 ;(async () => {
   console.log(await addNumbers(3, 4))
 })()
+
+// Variables and Values
+
+let age = 6; // => let age: number
+const majority = 18; // => const majority: 18
+
+// between 500 and 1000
+const RANDOM_WAIT_TIME =
+  Math.round(Math.random() * 500) + 500
+
+let startTime = new Date()
+// let endTime // => let endTime: any
+let endTime: Date // => let endTime: Date
+
+setTimeout(() => {
+  endTime = new Date()
+}, RANDOM_WAIT_TIME)
+
+function add(a: number, b: number) {
+  return a + b
+}
+
+const result = add(3, 4) // => const result: any
+const p = new Promise(result)
+// => Argument of type 'number' is not assignable to parameter of type
+// '(resolve: (value: unknown) => void, reject: (reason?: any) => void) => void | PromiseLike<void>'.
