68df9a60420a37a3f53d06accfac78b3aa32816b
Gwendal Le Bris <gwen.coding@gmail.com>
2023-10-31 20:38:45 +0100

    Lesson: Functions & Function Overloads

---
+++ packages/website/content/blog/fundamentals-v3/02-hello-typescript/lib/src/functions.ts
@@ -0,0 +1,61 @@
+interface TwoNumberCalculation {
+  (x: number, y: number): number
+}
+// Equivalent to
+type TwoNumberCalc = (x: number, y: number) => number
+
+const add: TwoNumberCalculation = (a, b) => a + b;
+
+const subtract: TwoNumberCalc = (a, b) => a - b;
+
+// Void vs Undefined
+
+function invokeInFourSeconds(callback: () => undefined) {
+  setTimeout(callback, 4000)
+}
+function invokeInFiveSeconds(callback: () => void) {
+  setTimeout(callback, 5000)
+}
+
+const values: number[] = []
+// invokeInFourSeconds(() => values.push(4))
+// => Type 'number' is not assignable to type 'undefined'.
+invokeInFiveSeconds(() => values.push(4))
+
+// Construct signatures
+
+interface DateConstructor {
+  new (value: number): Date
+}
+
+let MyDateConstructor: DateConstructor = Date
+const d = new MyDateConstructor()
+
+type FormSubmitHandler = (data: FormData) => void
+type MessageHandler = (evt: MessageEvent) => void
+
+// Function overloads
+
+function handleMainEvent(
+  elem: HTMLFormElement,
+  handler: FormSubmitHandler
+)
+function handleMainEvent(
+  elem: HTMLIFrameElement,
+  handler: MessageHandler
+)
+function handleMainEvent(
+  elem: HTMLFormElement | HTMLIFrameElement,
+  handler: FormSubmitHandler | MessageHandler
+) {}
+
+const myFrame = document.getElementsByTagName("iframe")[0]
+//     ^?
+const myForm = document.getElementsByTagName("form")[0]
+//     ^?
+handleMainEvent(myFrame, (val) => {
+  //        ^?
+})
+handleMainEvent(myForm, (val) => {
+  //        ^?
+})
--- packages/website/content/blog/fundamentals-v3/02-hello-typescript/lib/src/generics_scopes.ts
+++ packages/website/content/blog/fundamentals-v3/02-hello-typescript/lib/src/generics_scopes.ts
@@ -1,18 +1,18 @@
-interface HasId {
-  id: string
-}
-interface Dict<T> {
-  [k: string]: T
-}
+// interface HasId {
+//   id: string
+// }
+// interface Dict<T> {
+//   [k: string]: T
+// }
 
-// => Will not work :
-// function listToDict(list: HasId[]): Dict<HasId> {
-function listToDict<T extends HasId>(list: T[]): Dict<T> {
-  const dict: Dict<T> = {}
+// // => Will not work :
+// // function listToDict(list: HasId[]): Dict<HasId> {
+// function listToDict<T extends HasId>(list: T[]): Dict<T> {
+//   const dict: Dict<T> = {}
 
-  list.forEach((item) => {
-    dict[item.id] = item
-  })
+//   list.forEach((item) => {
+//     dict[item.id] = item
+//   })
 
-  return dict
-}
+//   return dict
+// }
