759457454ff36d933d66eb87a8ee5e7d4fadca87
Gwendal Le Bris <gwen.coding@gmail.com>
2023-10-31 21:12:46 +0100

    Lesson: Classes

--- packages/website/content/blog/fundamentals-v3/02-hello-typescript/lib/src/classes.ts
+++ packages/website/content/blog/fundamentals-v3/02-hello-typescript/lib/src/classes.ts
@@ -12,3 +12,49 @@
 // const d = new Dog()
 // console.log(d.eat("bacon"))
 // console.log(d.bark())
+
+class Voiture {
+  make: string
+  model: string
+  year: number
+  constructor(make: string, model: string, year: number) {
+    this.make = make
+    this.model = model
+    this.year = year
+  }
+}
+
+let sedan = new Voiture("Honda", "Accord", 2017)
+
+// sedan.activateTurnSignal("left") // not safe!
+// => Property 'activateTurnSignal' does not exist on type 'Voiture'.
+
+// new Voiture(2017, "Honda", "Accord") // not safe!
+// => Argument of type 'number' is not assignable to parameter of type 'string'.
+
+// Param Properties
+
+// class Drive {
+//   make: string
+//   model: string
+//   year: number
+//   constructor(make: string, model: string, year: number) {
+//     this.make = make
+//     this.model = model
+//     this.year = year
+//   }
+// }
+
+// +> TypeScript provides a more concise syntax for code like this,
+// through the use of param properties:
+class Drive {
+  constructor(
+    public make: string,
+    public model: string,
+    public year: number
+  ) {}
+}
+
+const myCar = new Drive("Honda", "Accord", 2017)
+myCar.make
+myCar.year
