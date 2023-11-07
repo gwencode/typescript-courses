04620e0d2c6944bad93170c51b6baa1b93f20010
gwendal le bris <gwen.coding@gmail.com>
2023-11-07 09:51:34 +0100

    CONDITIONAL TYPES QUIZ

--- packages/website/content/blog/intermediate-v1/lib/src/05_conditional_types.ts
+++ packages/website/content/blog/intermediate-v1/lib/src/05_conditional_types.ts
@@ -15,3 +15,26 @@ let device1: CookingDevice<"grill">
 //   ^? let device1: Grill
 let device2: CookingDevice<"oven">
 //   ^? let device2: Oven
+
+
+// CONDITIONAL TYPES QUIZ
+
+type answer_1 = 64 extends number ? true : false
+//     ^? true
+type answer_2 = number extends 64 ? true : false
+//     ^? false
+type answer_3 = string[] extends any ? true : false
+//     ^? true
+type answer_4 = string[] extends any[] ? true : false
+//     ^? true
+type answer_5 = never extends any ? true : false
+//     ^? true
+type answer_6 = any extends any ? true : false
+//     ^? true
+// prettier-ignore
+type answer_7 = Date extends { new (...args: any[]): any } ?  true : false
+//     ^? false
+
+// prettier-ignore
+type answer_8 = typeof Date extends { new (...args: any[]): any } ?  true : false
+//     ^? true
