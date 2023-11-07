2686f316e6fb123c58ce07b090c14099cac29c81
gwendal le bris <gwen.coding@gmail.com>
2023-11-07 11:41:38 +0100

    Lesson Advanced Mapped Types

--- packages/website/content/blog/intermediate-v1/lib/src/09_mapped_types.ts
+++ packages/website/content/blog/intermediate-v1/lib/src/09_mapped_types.ts
@@ -1,3 +1,5 @@
+// EXAMPLE
+
 type MyFruit = {
   name: string
   color: string
@@ -14,7 +16,6 @@ function printFruitCatalog(fruitCatalog: ExampleRecord) {
   // fruitCatalog.pineapple // => La propriété 'pineapple' n'existe pas sur le type 'ExampleRecord'.ts(2339)
 }
 
-
 // GENERALIZE:
 
 // - type MyRecord = { [FruitKey in "apple" | "cherry"]: Fruit }
@@ -23,3 +24,46 @@ function printFruitCatalog(fruitCatalog: ExampleRecord) {
 type MyRecord<KeyType extends string, ValueType> = {
   [Key in KeyType]: ValueType
 }
+
+
+// ADVANCED MAPPED TYPES
+
+// EXAMPLE
+
+type ExartOfWindow = {
+  [Key in
+  | "document"
+  | "navigator"
+  | "setTimeout"]: Window[Key]
+}
+// ^? => type ExPartOfWindow = {
+//   document: Document;
+//   navigator: Navigator;
+//   setTimeout: (handler: TimerHandler, timeout?: number, ...arguments: any[]) => number;
+// }
+
+// GENERALIZE TYPE PARAMS:
+
+type PickWindowProperties<Keys extends keyof Window> = {
+  [Key in Keys]: Window[Key]
+}
+
+type PartOfWindow = PickWindowProperties<"document" | "navigator" | "setTimeout">
+//   ^? type PartOfWindow = {
+//     document: Document;
+//     navigator: Navigator;
+//     setTimeout: (handler: TimerHandler, timeout?: number | undefined, ...arguments: any[]) => number;
+// }
+
+// GENERALIZE TYPE :
+
+type PickProperties<ValueType, Keys extends keyof ValueType> = {
+  [Key in Keys]: ValueType[Key]
+}
+
+type PartOfWindow2 = PickProperties<Window, "document" | "navigator" | "setTimeout">
+// ^? type PartOfWindow2 = {
+//     readonly document: Document;
+//     readonly navigator: Navigator;
+//     setTimeout: (handler: TimerHandler, timeout?: number | undefined, ...arguments: any[]) => number;
+// }
