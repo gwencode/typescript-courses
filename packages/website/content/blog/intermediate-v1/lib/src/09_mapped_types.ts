aaf8f3a2a10d81c93c024ea0de00fca1d5c1bb8b
gwendal le bris <gwen.coding@gmail.com>
2023-11-07 12:11:48 +0100

    Lesson Template Literal Types & Key Mapping

--- packages/website/content/blog/intermediate-v1/lib/src/09_mapped_types.ts
+++ packages/website/content/blog/intermediate-v1/lib/src/09_mapped_types.ts
@@ -25,12 +25,11 @@ type MyRecord<KeyType extends string, ValueType> = {
   [Key in KeyType]: ValueType
 }
 
-
 // ADVANCED MAPPED TYPES
 
 // EXAMPLE
 
-type ExartOfWindow = {
+type ExPartOfWindow = {
   [Key in
   | "document"
   | "navigator"
@@ -90,3 +89,40 @@ type PartOfWindow2 = PickProperties<Window, "document" | "navigator" | "setTimeo
 // type Readonly<T> = {
 //   readonly [P in keyof T]: T[P]
 // }
+
+
+// TEMPLATE LITERAL TYPES & KEY MAPPING
+
+type ArtFeatures = "cabin" | "tree" | "sunset"
+type Colors =
+  | "darkSienna"
+  | "sapGreen"
+  | "titaniumWhite"
+  | "prussianBlue"
+
+type ArtMethodNames = `paint${Capitalize<Colors>}${Capitalize<ArtFeatures>}`
+// ^? type ArtMethodNames = "paintDarkSiennaCabin" | "paintDarkSiennaTree" |
+// "paintDarkSiennaSunset" | "paintSapGreenCabin" | "paintSapGreenTree" |
+// "paintSapGreenSunset" | "paintTitaniumWhiteCabin" | ... 4 more ... | "paintPrussianBlueSunset"
+
+interface DataState {
+  digits: number[]
+  names: string[]
+  flags: Record<"darkMode" | "mobile", boolean>
+}
+
+type DataSDK = {
+  // The mapped type
+  [K in keyof DataState as `set${Capitalize<K>}`]:
+    (arg: DataState[K]) => void
+}
+
+function load(dataSDK: DataSDK) {
+  dataSDK.setDigits([14])
+  dataSDK.setFlags({ darkMode: true, mobile: false })
+  // dataSDK.setFlags({ darkMode: true, mobil: false }) => SPELL ERROR
+  // => Object literal may only specify known properties,
+  // // but 'mobil' does not exist in type
+  // 'Record<"darkMode" | "mobile", boolean>'.
+  // Did you mean to write 'mobile'?
+}
